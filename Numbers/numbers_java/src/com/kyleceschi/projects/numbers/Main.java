88b1728a96f798d787c560e7b76480e6956db2c8
Kyle Ceschi <ceschikt18@gmail.com>
2013-08-28 10:13:10 -0500

    Create new project for numbers solutions

---
+++ Numbers/numbers_java/src/com/kyleceschi/projects/numbers/Main.java
@@ -0,0 +1,15 @@
+package com.kyleceschi.projects.numbers;
+
+/**
+ * Main entry point for numbers project
+ * User: kceschi
+ * Date: 8/28/13
+ * Time: 10:05 AM
+ *
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
